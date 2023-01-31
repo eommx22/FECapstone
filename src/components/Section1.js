bbbb1bf39f327bca8813771e27a030a503e28266
eommx22 <eo.22.mmx@gmail.com>
2023-01-31 17:20:12 +0100

    section1,2 css

--- src/components/Section1.js
+++ src/components/Section1.js
@@ -5,7 +5,7 @@ const Section1 = () => {
         <h1>Little Lemon</h1>
         <h3 id="Chicago">Chicago</h3>
         <p id="description">Little Lemon is a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
-        <button type="submit" id="reserv_btn" class="yellowbtn">Reserve a table</button>
+        <button type="button" id="reserv_btn" class="yellowbtn">Reserve a table</button>
         </div>
         <div id="hero_rightside">
           <img src="../assets/img/restaurantfood.jpg" alt='restaurant_food'/>
--- src/components/Section2.js
+++ src/components/Section2.js
@@ -1,7 +1,42 @@
 const Section2= () => {
     return (
       <section id="highlights">
-        <h1>Section2</h1>
+        <div id="row1">
+           <h2>This week specials!</h2>
+           <button type="button" >Online Order</button>
+        </div>
+        <div id="row2">
+          <section id="card1" class="card">
+            <img src="../assets/img/greek salad.jpg" alt="salad"/>
+            <div class="row3">
+               <span class="left">Greek salad</span>
+               <span class="right">$ 10.75</span>
+            </div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla pharetra,
+               lorem nec interdum finibus, augue.</p>
+            <button type="button">Order a delivery</button>
+          </section>
+          <section id="card2" class="card">
+            <img src="../assets/img/Bruschetta.jpg" alt="bruschetta"/>
+            <div class="row3">
+                <span class="left">Bruschetta</span>
+                <span class="right">$ 8.50</span>
+            </div>
+            <p >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla pharetra,
+               lorem nec interdum finibus, augue.</p>
+            <button type="button">Order a delivery</button>
+          </section>
+          <section id="card3" class="card">
+            <img src="../assets/img/lemon dessert.jpg" alt="lemondessert"/>
+            <div class="row3">
+               <span class="left">Lemon Dessert</span>
+               <span class="right">$ 9.45</span>
+            </div>
+            <p >Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+              Nulla pharetra, lorem nec interdum finibus, augue.</p>
+            <button type="button">Order a delivery</button>
+          </section>
+        </div>
       </section>
     );
   };
