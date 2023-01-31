40bb12c210e8f5800fcbff8812529ebe7ccb632c
eommx22 <eo.22.mmx@gmail.com>
2023-01-31 18:08:10 +0100

    section 2,3 css

--- src/components/Section3.js
+++ src/components/Section3.js
@@ -1,7 +1,33 @@
 const Section3 = () => {
     return (
       <section id="testimonials">
-        <h1>Section3</h1>
+        <h3>Testimonials</h3>
+        <div id="rating">
+           <div class="rating-card">
+               <span class="card-title">Rating</span>
+               <img src="../assets/img/photo1.png" alt="photo1.jpg"/>
+               <span class="uname"> Name</span>
+               <p>review</p>
+            </div> 
+            <div class="rating-card">
+               <span class="card-title">Rating</span>
+               <img src="../assets/img/photo2.png" alt="photo2.jpg"/>
+               <span class="uname"> Name</span>
+               <p>review</p>
+            </div> 
+            <div class="rating-card">
+               <span class="card-title">Rating</span>
+               <img src="../assets/img/photo3.png" alt="photo3.jpg"/>
+               <span class="uname"> Name</span>
+               <p>review</p>
+            </div> 
+            <div class="rating-card">
+               <span class="card-title">Rating</span>
+               <img src="../assets/img/photo4.png" alt="photo4.jpg"/>
+               <span class="uname"> Name</span>
+               <p>review</p>
+            </div>  
+        </div>      
       </section>
     );
   };
